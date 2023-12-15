9ba1c9968245faf4fc3cdae5e81157dc15f7b9c6
s酱 <suzuki1968@protonmail.com>
2023-12-15 21:59:40 +0800

    finish exlab1

--- src/tcp.c
+++ src/tcp.c
@@ -4,7 +4,7 @@
 
 SOCKET socketServer;
 
-//处理关闭连接
+//处理关闭连接，即如果检测到SIGINT，即清理套接字相关的资源，终止通信
 void sig_handler(int sig) {
   if(sig == SIGINT) {
     printf("connection close");
@@ -25,22 +25,24 @@ int tcp() {
   char recvBuf[RECV_BUFFER_LENGTH];
   char *sendBuf;
 
+  //初始化Winsock库，指定使用的版本为2.2
   WSAStartup(MAKEWORD(2, 2), &wsaData);
+  //创建一个TCP套接字，并将其赋值给socketServer
   socketServer = socket(AF_INET, SOCK_STREAM, 0);
+  //将服务器套接字绑定到本地的IP地址，这里使用0.0.0.0表示绑定到所有可用的网络接口
   addrServer.sin_addr.S_un.S_addr = inet_addr("0.0.0.0");
   addrServer.sin_family = AF_INET; // IPv4
-  addrServer.sin_port = htons(80);
+  addrServer.sin_port = htons(80); //Port 80
   bind(socketServer, (SOCKADDR*)&addrServer, sizeof(SOCKADDR));
   
   if(listen(socketServer, BACKLOG_LENGTH) == 0) {
     printf("[Info] Start to listen 0.0.0.0:80\r\n");
-    signal(SIGINT, sig_handler);
+    signal(SIGINT, sig_handler);//注册SIGINT信号的处理函数，即按下Ctrl+C时执行的函数。
     while(1) {
-      printf("this is a debug msg");
+      //接受客户端的连接请求，返回一个新的套接字socketClient用于与客户端通信
       socketClient = accept(socketServer, (SOCKADDR*)&addrClient, &addrlen);
       if(socketClient != -1) {
-        recv(socketClient, recvBuf, RECV_BUFFER_LENGTH, 0);
-        
+        recv(socketClient, recvBuf, RECV_BUFFER_LENGTH, 0); 
         sendBuf = http_process(recvBuf);
         if(sendBuf) {
           send(socketClient, sendBuf, strlen(sendBuf), 0);
